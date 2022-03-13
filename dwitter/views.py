baf2e15978b1e2fa14a80121e1e5473410b96f85
floatingnahid22 <nahid.uap.github@gmail.com>
2022-03-14 01:50:15 +0600

    update views.py of dwitter apps
    With this code, I am defining the view function that’ll handle all requests to the /profile_list URL slug

--- dwitter/views.py
+++ dwitter/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Profile
 
 # Create your views here.
 
 def dashboard(request):
     return render(request, "base.html")
+
+
+def profile_list(request):
+    profiles  = Profile.objects.execlude(user=request.user)
+    return render(request, "dwitter/profile_list.html", {"profiles":profiles} )    
