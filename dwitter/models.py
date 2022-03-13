3da545d89dc4353bcb87e166761b8a6016df5662
floatingnahid22 <nahid.uap.github@gmail.com>
2022-03-14 00:24:35 +0600

    models.py updated
    Django comes with a receiver decorator, which allows you to make the code you wrote more concise, without changing its functionality

--- dwitter/models.py
+++ dwitter/models.py
@@ -1,8 +1,7 @@
-
-
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -15,12 +14,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
-
+        
+    @receiver(post_save, sender=User)
     def create_profile(sender, instance, created, **kwargs):
         if created:
             user_profile  = Profile(user=instance)
             user_profile.save()
             user_profile.follows.add([instance.profile])
             user_profile.save()
-    #create a Profile for each new user.        
-    post_save.connect(create_profile, sender=User)            
+    
