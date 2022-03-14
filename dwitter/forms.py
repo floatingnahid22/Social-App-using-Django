1ad49be55b389fc42dd660662419034275e502b9
floatingnahid22 <nahid.uap.github@gmail.com>
2022-03-14 16:47:35 +0600

    update forms.py
    improve the display of Django form by adding customizations through a widget to forms.CharField in forms.py

--- dwitter/forms.py
+++ dwitter/forms.py
@@ -2,7 +2,16 @@ from django import forms
 from .models import Dweet
 
 class DweetForm(forms.ModelForm):
-    body     = forms.CharField(required=True)
+    body = forms.CharField(
+        required=True,
+        widget=forms.widgets.Textarea(
+            attrs={
+                "placeholder": "Dweet something...",
+                "class": "textarea is-success is-medium",
+            }
+        ),
+        label="",
+    )
 
     class Meta:
         model   = Dweet
